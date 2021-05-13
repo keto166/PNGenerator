87e3c19d5e19dd98a3ae97733d73b4c244707e5e
Keira Ryan <ryanto166@gmail.com>
2021-05-13 09:58:20 -0400

    Add project files.

---
+++ PNGenerator/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace PNGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.cbCats = new System.Windows.Forms.ComboBox();
+            this.flCount = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Push";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbCats
+            // 
+            this.cbCats.FormattingEnabled = true;
+            this.cbCats.Location = new System.Drawing.Point(68, 56);
+            this.cbCats.Name = "cbCats";
+            this.cbCats.Size = new System.Drawing.Size(323, 23);
+            this.cbCats.TabIndex = 1;
+            this.cbCats.SelectedIndexChanged += new System.EventHandler(this.cbCats_SelectedIndexChanged);
+            // 
+            // flCount
+            // 
+            this.flCount.AutoSize = true;
+            this.flCount.Location = new System.Drawing.Point(398, 63);
+            this.flCount.Name = "flCount";
+            this.flCount.Size = new System.Drawing.Size(38, 15);
+            this.flCount.TabIndex = 2;
+            this.flCount.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Category";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Title";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(68, 85);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(323, 23);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Type";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(68, 27);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(323, 23);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flCount);
+            this.Controls.Add(this.cbCats);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cbCats;
+        private System.Windows.Forms.Label flCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ PNGenerator/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections;
+
+namespace PNGenerator
+{
+    public partial class Form1 : Form
+    {
+        Excel.Application objApp;
+        Excel.Workbooks objBooks;
+        Excel.Sheets objSheets;
+        Excel._Worksheet objSheet;
+        Excel.Workbook objBook;
+        Excel.Range objRange;
+
+        Dictionary<String, int> cats;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            this.button1.Text = "GHeyyy";
+            //MessageBox.Show("Heyy");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                // Instantiate Excel and start a new workbook.
+                ArrayList exInst = new ArrayList();  //List of existing Excel
+                Process[] P_CESSES = Process.GetProcessesByName("EXCEL");
+                for (int p_count = 0; p_count < P_CESSES.Length; p_count++)
+                {
+                    exInst.Add(P_CESSES[p_count].Id);
+                        
+                    
+
+                }
+                objApp = new Excel.Application();
+
+                objBooks = objApp.Workbooks;
+                objBook = objBooks.Open("Y:\\Mechanical\\Inventor\\iLogic_Plug_Ins\\iLogic_Drawings_Plug_Ins\\TestXL.xlsx");
+                objSheets = objBook.Worksheets;
+                objSheet = (Excel._Worksheet)objSheets.get_Item(1);
+
+
+
+
+                objSheet.Cells[2, 1] = "Heyy";
+
+                
+                Boolean bEndFound = false;
+                int n = 0;
+                String tempString;
+                int tempCount = 0;
+                if (cats == null) { cats = new Dictionary<string, int>(); } else { cats.Clear(); }
+                ComboBox _cbCats = cbCats;
+
+                while (!bEndFound)
+                {
+                    objRange = ((Excel.Range)objSheet.Cells[3 + n, 4]);
+                    tempString = objRange.Text;
+                    if (tempString.Equals("")) {
+                        bEndFound = true;
+                    } else
+                    {
+                        objRange = ((Excel.Range)objSheet.Cells[3 + n, 5]);
+                        tempCount = Convert.ToInt32(objRange.Value2);
+                        cats.Add(tempString, tempCount);
+                        n++;
+                        _cbCats.Items.Add(tempString);
+                    }
+                }
+
+                
+
+                objBook.Save();
+                objBook.Close(0);
+
+                Boolean bFound = false;
+                P_CESSES = Process.GetProcessesByName("EXCEL");
+                for (int p_count = 0; p_count < P_CESSES.Length; p_count++)
+                {
+                    bFound = false;
+                    foreach (int s in exInst)
+                    {
+                        if (s.Equals(P_CESSES[p_count].Id)) {
+                            bFound = true;
+                        }
+                    }
+
+                    if (!bFound)
+                    {
+                        P_CESSES[p_count].Kill();
+                    }
+                }
+
+                while (System.Runtime.InteropServices.Marshal.FinalReleaseComObject(objApp) != 0) { }
+                while (System.Runtime.InteropServices.Marshal.FinalReleaseComObject(objBooks) != 0) { }
+                while (System.Runtime.InteropServices.Marshal.FinalReleaseComObject(objBook) != 0) { }
+                while (System.Runtime.InteropServices.Marshal.FinalReleaseComObject(objSheets) != 0) { }
+                while (System.Runtime.InteropServices.Marshal.FinalReleaseComObject(objSheet) != 0) { }
+                objApp = null;
+                objBooks = null;
+                objBook = null;
+                objSheets = null;
+                objSheet = null;
+                
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+            }
+            catch (Exception myError)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, myError.Message);
+                errorMessage = String.Concat(errorMessage, " Line: ");
+                errorMessage = String.Concat(errorMessage, myError.Source);
+
+                MessageBox.Show(errorMessage, "Error");
+            }
+
+            
+        }
+
+        private void cbCats_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String temp = (String)cbCats.SelectedItem;
+            flCount.Text = cats[temp].ToString("D5");
+        }
+    }
+}
